8a18e036370418582e375b9e9d853925bf9cc322
Bonsa Adugna Mosisa <bonsa.adugnam@gmail.com>
2024-04-28 15:10:00 +0300

    first commit

---
+++ FuncJava.java
@@ -0,0 +1,10 @@
+public class FuncJava {
+    String Fname="Bonsa";
+    String Lname="Adugna";
+    int age=19;
+    public static void main(String args[]){
+        FuncJava Obj1=new FuncJava();
+        System.out.println("NAME :"+ Obj1.Fname+ " "+ Obj1.Lname);
+        System.out.println("AGE :"+ Obj1.age);
+    }
+}
